a43ea549201a6d181e17aed752679d0ff8e52c57
Gabriel Torcat <87126387+gabr1elt@users.noreply.github.com>
2023-04-20 17:11:58 +0000

    last changes index

--- gabrieltorcat/src/pages/IndexPage.vue
+++ gabrieltorcat/src/pages/IndexPage.vue
@@ -1,37 +1,52 @@
 <template>
   <q-page class="row items-center justify-center bg-primary">
-    <q-card class="q-page-outer bg-primary" flat square>
+
+    <q-card class="q-page-outer bg-primary q-px-xl" flat square>
+
+      <!-- Picture -->
       <q-card-section class="row items-center justify-center">
-        <q-avatar size="100%" color="secondary">
-          <img src="~assets/images/Portrait_silouette_small.png" />
-          <!-- <img src="~assets/images/Portrait_circle_small.png"> -->
-        </q-avatar>
+        <div style="width: 45%; aspect-ratio: 1; min-width: 250px;">
+          <q-avatar style="width: 100%; height: 100%;" color="secondary">
+            <img src="~assets/images/Portrait_silouette_small.png" />
+            <!-- <img src="~assets/images/Portrait_circle_small.png"> -->
+          </q-avatar>
+        </div>
       </q-card-section>
 
+      <!-- Title -->
       <q-card-section>
-        <h1 :class="$q.screen.gt.md ? 'text-h1' : 'text-h2'" class="text-center text-secondary">
+        <h1 :class="$q.screen.gt.sm ? 'text-h1 q-my-lg' : 'text-h2 q-my-md'" class="text-center text-secondary">
           Gabriel Torcat
         </h1>
-        <div :class="$q.screen.gt.md ? 'text-subtitle1' : 'text-subtitle2'" class="text-center text-secondary">
+        <div :class="$q.screen.gt.sm ? 'text-subtitle1 q-my-md' : 'text-subtitle2 q-my-sm'"
+          class="text-center text-secondary">
           {{ subTitle }}
         </div>
       </q-card-section>
 
+      <!-- Buttons -->
       <q-card-actions align="around">
-        <q-btn round color="secondary" text-color="primary" size="lg" icon="mail" />
-        <q-btn round color="secondary" text-color="primary" size="lg" icon="description" />
-        <q-btn round color="secondary" text-color="primary" size="lg" icon="fa-brands fa-linkedin-in" />
+        <q-btn round color="secondary" text-color="primary" :size="$q.screen.gt.sm ? 'lg' : 'md'" icon="mail" />
+        <q-btn round color="secondary" text-color="primary" :size="$q.screen.gt.sm ? 'lg' : 'md'" icon="description" />
+        <q-btn round color="secondary" text-color="primary" :size="$q.screen.gt.sm ? 'lg' : 'md'"
+          icon="fa-brands fa-linkedin-in" />
+        <q-btn round color="secondary" text-color="primary" :size="$q.screen.gt.sm ? 'lg' : 'md'"
+          icon="fa-regular fa-microchip-ai" />
       </q-card-actions>
 
-      <q-card-section>
-        <h2 :class="$q.screen.gt.md ? 'text-h2' : 'text-h3'" class="text-left text-secondary">
+      <!-- Profile -->
+      <q-card-section class="">
+        <h2 :class="$q.screen.gt.sm ? 'text-h2 q-my-lg' : 'text-h3 q-my-md'" class="text-left text-secondary">
           Profile
         </h2>
-        <div :class="$q.screen.gt.md ? 'text-body1' : 'text-body2'" class="text-center text-justify text-secondary">
+        <div :class="$q.screen.gt.sm ? 'text-body1' : 'text-body2'" class="text-center text-justify text-secondary"
+          style="text-indent: 30px">
           {{ profile }}
         </div>
       </q-card-section>
+
     </q-card>
+
   </q-page>
 </template>
 
@@ -50,7 +65,6 @@
 <script setup>
 
 const subTitle = "INNOVATION / TECHNOLOGY / X06";
-// const profile2 = 
 const profile =
   `Firm promoter of Innovation and the Work Smarter and Not Harder Principle. I don't Believe in Problems, I Believe in Solutions. 
   A scientist at heart, I apply critical and strategic thinking to materialize solid and cutting-edge business solutions to improve performance, 
